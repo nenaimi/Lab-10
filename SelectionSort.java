8abe50c6054a9531716a7407f78b135e895fd327
Nurra <nenaimi@umich.edu>
2020-12-03 00:08:55 -0500

    Positives and Negatives tested

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,30 +1,34 @@
 package Lab10;
 
 public class SelectionSort {
-	 private int temp;
 
 	/** Creates a new instance of SelectionSort */
-    public SelectionSort() {
-    }
+   public SelectionSort() {
+   }
 
-    /* A simple SelectionSort algorithm
-     * pre-condition:
-     * post-condition:
-     * inputs:
-     * outputs:
-     * special conditions:
-     */
-    public int[] basicSelectionSort(int[] x) {
-        for (int i = 0; i < x.length; ++i) {
-            for (int j= i+1; j < x.length; ++j) {
-                if (x[i] > x[j]) {
-                    temp = x[i];
-                    x[i] = x[j];
-                    temp = x[j];
-                }
-            } // end of inner for loop
-        } // end of outer for loop
-        return x;
-    } // end of basicSelectionSort method
+   /* A simple SelectionSort algorithm
+    * pre-condition: have an unsorted int array
+    * post-condition: returns a sorted array (least to greatest)
+    * inputs: int array
+    * outputs: int array
+    * special conditions: none
+    */
+   public int[] basicSelectionSort(int[] x) {
+       for (int i = 0; i < x.length; ++i) {
+
+    	   int index = i;
+
+           for (int j= i+1; j < x.length; ++j) {
+               if (x[index] > x[j]) {
+                   index = j;
+               }
+           }
+
+           int small = x[index];
+           x[index] = x[i];
+           x[i] = small;
+       }
+       return x;
+   }
 
 }
